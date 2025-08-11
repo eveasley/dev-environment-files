e2e6261158ce8a1eb6352f455050f2c704ed76e9
EJ Veasley <earnestveasley@Mac.lan>
2025-08-11 12:36:38 -0400

    fixed config

--- utils/alb.tf
+++ utils/alb.tf
@@ -4,7 +4,7 @@ locals {
 
 resource "aws_security_group" "alb" {
   name        = "${var.name_prefix}-${var.environment}-alb-sg"
-  description = "Allow HTTP from Zscaler only"
+  description = "Allow HTTP/HTTPS from Zscaler only"
   vpc_id      = var.vpc_id
 
   ingress {
@@ -12,6 +12,15 @@ resource "aws_security_group" "alb" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = length(var.zscaler_ips) > 0 ? var.zscaler_ips : ["0.0.0.0/0"]
+    description = "Allow HTTP from Zscaler only"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = length(var.zscaler_ips) > 0 ? var.zscaler_ips : ["0.0.0.0/0"]
+    description = "Allow HTTPS from Zscaler only"
   }
 
   egress {
@@ -59,18 +68,34 @@ resource "aws_lb_target_group" "loki" {
   }
 
   tags = merge(
-  var.tags,
-  {
-    Name = "${var.name_prefix}-${var.environment}-alb-sg"
-  }
-)
-
+    var.tags,
+    {
+      Name = "${var.name_prefix}-${var.environment}-loki-tg"
+    }
+  )
 }
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.app.arn
   port              = 80
   protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      protocol    = "HTTPS"
+      port        = "443"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.acm_certificate_arn
 
   default_action {
     type             = "forward"
@@ -83,9 +108,19 @@ resource "aws_security_group_rule" "allow_http_from_alb" {
   from_port                = 80
   to_port                  = 80
   protocol                 = "tcp"
-  security_group_id        = local.ecs_tasks_sg_id           
-  source_security_group_id = aws_security_group.alb.id     
+  security_group_id        = local.ecs_tasks_sg_id
+  source_security_group_id = aws_security_group.alb.id
   description              = "Allow HTTP from ALB"
+}
+
+resource "aws_security_group_rule" "allow_https_from_alb" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = local.ecs_tasks_sg_id
+  source_security_group_id = aws_security_group.alb.id
+  description              = "Allow HTTPS from ALB"
 }
 
 resource "aws_security_group_rule" "allow_loki_from_alb" {
